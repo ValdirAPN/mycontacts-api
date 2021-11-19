f37b071045a79d22ce2327629bf13d8b852d8c61
valdirpn <valdiraires.pn@gmail.com>
2021-11-18 21:57:46 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+require('express-async-errors');
+
+const routes = require('./routes');
+const cors = require('./app/middlewares/cors');
+const errorHandler = require('./app/middlewares/errorHandler');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors);
+app.use(routes);
+app.use(errorHandler);
+
+const port = 3001;
+app.listen(port, () => console.log(`Server started at http://localhost:${port}`));
